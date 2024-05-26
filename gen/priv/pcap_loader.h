02ba42863c0c33d84ed499948fde1948f26fcbaf
iva <ivayla@baruh.net>
2024-05-26 18:20:59 +0300

    mempool

--- gen/priv/pcap_loader.h
+++ gen/priv/pcap_loader.h
@@ -12,7 +12,6 @@ typedef struct tgn_pcap_pkt_hdr {
 
 bool check_pcap(FILE* pcap_file);
 struct rte_mbuf* get_packet(FILE* pcap_file, struct rte_mempool* mbuf_pool);
-uint32_t load_packets(struct rte_mbuf** tx_packets, FILE* pcap_file, struct rte_mempool* mbuf_pool)
+uint32_t load_packets(struct rte_mbuf** tx_packets, FILE* pcap_file, struct rte_mempool* mbuf_pool);
 
 #endif //TRAFFIC_GENERATOR_PCAP_LOADER_H
-
